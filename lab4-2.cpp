b876e8b0d40eabb937e9f98c4da52b9ed488ff25
TherealKyleG <norunningintemplez@gmail.com>
2025-02-10 19:53:45 -0500

    Update lab4-2.cpp

--- lab4-2.cpp
+++ lab4-2.cpp
@@ -2,9 +2,16 @@
 //Philip Regalbuto
 #include <iostream>
 using namespace std;
+
 int main()
 {
-
+ const double COMM_RATE =0.1;
+ double sales =0.0;
+ double commission = 0.0;
+  cout<<"Sales Amount: ";
+  cin >> sales;
+  commission = sales * COMM_RATE;
+  cout<< "Commission: $" + commission<< endl;
 
   return 0;
 } //end of main function
